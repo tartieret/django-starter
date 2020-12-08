2e782c7c91775e3e8313d34d16dbe3ed6ac2d741
Thomas Tartiere <thomas.tartiere@panevo.com>
2020-12-08 15:21:20 -0800

    fix: Create/Edit users from the admin

--- app/users/admin.py
+++ app/users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth import admin as auth_admin
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 
 from app.users.forms import UserChangeForm, UserCreationForm
 
@@ -12,9 +13,20 @@ class UserAdmin(auth_admin.UserAdmin):
 
     form = UserChangeForm
     add_form = UserCreationForm
-    fieldsets = (("User", {"fields": ("name",)}),) + tuple(
-        auth_admin.UserAdmin.fieldsets
+    fieldsets = (("User", {"fields": ("name",)}),) +     (
+        (None, {'fields': ('email', 'password')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+
     list_display = ["email", "name", "is_superuser"]
     search_fields = ["name"]
     ordering = ["email"]
